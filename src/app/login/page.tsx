82e44d86576ccb3b6e45172990a6400398c9d690
pphatdev <leatsophat02@gmail.com>
2024-04-29 15:00:45 +0700

    Update datatable

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,27 +1,29 @@
-import * as React from "react"
+"use client"
 import {
     Card,
     CardContent,
     CardDescription,
     CardFooter,
     CardHeader,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "@/components/elements/card"
+import { Input } from "@/components/elements/input"
+import { Label } from "@/components/elements/label"
 import { Container } from '@/components/container';
 import { StarBoard } from '@/components/stars';
 import Link from "next/link"
-import LoginButton from "@/components/ui/login-button";
+import LoginButton from "@/components/elements/login-button";
+import { useState } from "react";
 
 export default function Login() {
-
+    const [email, setEmail] = useState('test@pphat.me')
+    const [password, setPassword] = useState('123')
     return(
         <StarBoard>
-            <div className="absolute -z-10 inset-0 overflow-hidden opacity-20">
+            <div className="absolute z-1 inset-0 overflow-hidden opacity-20">
                 <div className="jumbo absolute -inset-[10px] opacity-50"></div>
             </div>
             <Container>
-                <div className='h-screen flex items-center justify-center font-poppins text-white'>
+                <div className='dark h-screen flex items-center justify-center font-poppins text-white'>
                 <Card className="max-w-sm px-3 py-6 w-full bg-white/5 backdrop-blur-[2px] border-0 dark">
                     <CardHeader>
                         <div>
@@ -41,18 +43,18 @@ export default function Login() {
                             <div className="grid w-full items-center gap-6">
                                 <div className="flex flex-col space-y-1.5">
                                     <Label className="text-white" htmlFor="email">Email</Label>
-                                    <Input value="admin@pphat.me" className="bg-transparent text-slate-200 focus-visible:ring-slate-800 focus-visible:outline-none" id="email" placeholder="info.name@domain.com"/>
+                                    <Input value={email} onInput={(e: any)=> setEmail(e.target.value)} className="bg-transparent text-slate-200 focus-visible:ring-slate-800 focus-visible:outline-none" id="email" placeholder="info.name@domain.com"/>
                                 </div>
                                 <div className="flex flex-col space-y-1.5">
                                     <Label className="text-white" htmlFor="password">Password</Label>
-                                    <Input type="password" value="12345678" className="bg-transparent text-slate-200 focus-visible:ring-slate-800 focus-visible:outline-none" id="password" placeholder="Password"/>
+                                    <Input type="password" value={password} onInput={(e: any)=> setPassword(e.target.value)} className="bg-transparent text-slate-200 focus-visible:ring-slate-800 focus-visible:outline-none" id="password" placeholder="Password"/>
                                 </div>
                             </div>
                         </form>
                     </CardContent>
                     <CardFooter className="flex gap-2 justify-end mt-3">
                         <Link href="/" className="transition-all bg-transparent hover:bg-white/5 text-white ring-1 ring-inset ring-primary/10 py-2 px-5 inline-flex items-center justify-center focus:outline-none whitespace-nowrap rounded-md text-sm font-medium">Back</Link>
-                        <LoginButton goto={'/admin/dashboard'}/>
+                        <LoginButton goto={'/admin/projects/users'}/>
                     </CardFooter>
                 </Card>
                 </div>
